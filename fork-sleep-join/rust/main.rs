4e54045f3514970db006699cb575541d5e3ce885
root <root@DESKTOP-3K067O8.localdomain>
2022-01-09 22:22:29 -0300

    Add rust solution for fork sleep join exercise

---
+++ fork-sleep-join/rust/main.rs
@@ -0,0 +1,41 @@
+use std::{io::stdin, thread, time::Duration};
+use rand::Rng;
+
+fn main() {
+    println!("Type a number n below");
+
+    let n: i32 = input_number();
+
+    create_threads_and_wait(n);
+
+    println!("The number that you've been wait for: {}", n);
+}
+
+fn input_number() -> i32 {
+    let mut input_line = String::new();
+    stdin()
+        .read_line(&mut input_line)
+        .expect("Failed to read line");
+    let n: i32 = input_line
+        .trim().parse()
+        .expect("Input is not an integer");
+    return n;
+}
+
+fn create_threads_and_wait(n: i32) {
+    let capacity = n.try_into().unwrap();
+    let mut handles = Vec::with_capacity(capacity);
+    for i in 1..=n { 
+        handles.push(thread::spawn(move || {
+            let mut rng = rand::thread_rng();
+            let duration : Duration = Duration::from_secs(rng.gen_range(0..=5));
+            println!("thread number {} will sleep for {} seconds", n - i, duration.as_secs());
+            thread::sleep(duration);
+            println!("thread number {} woke up", n - i);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+}
