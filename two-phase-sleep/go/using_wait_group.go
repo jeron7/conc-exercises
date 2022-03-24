06f3837fe4a35fb1aabbfa379d0b7930970495fa
Jerônimo Jairo <jeronimo.araujo@ccc.ufcg.edu.br>
2022-03-24 18:43:24 -0300

    Refactor some variables names and close channels

--- two-phase-sleep/go/using_wait_group.go
+++ two-phase-sleep/go/using_wait_group.go
@@ -7,66 +7,76 @@ import (
 	"time"
 )
 
+const (
+	random_upper_bound int = 6
+)
+
 func main() {
 	fmt.Printf("Choose a number: ")
-	var n int
-	fmt.Scanf("%d", &n)
+	n := read_int()
 	start_threads_and_wait(n)
 	fmt.Printf("\nThe choosen number was: %d\n", n)
 }
 
+func read_int() int {
+	var value int
+	fmt.Scanf("%d", &value)
+	return value
+}
+
 func start_threads_and_wait(n int) {
+	channels := make([]chan int, n)
+	for i := 0; i < n; i++ {
+		channels[i] = make(chan int, 1)
+	}
 	f_barrier := &sync.WaitGroup{}
 	f_barrier.Add(n)
 	s_barrier := &sync.WaitGroup{}
 	s_barrier.Add(n)
-	ch := make([]chan int, n)
+
 	for i := 0; i < n; i++ {
-		chn := make(chan int, 1)
-		ch[i] = chn
-	}
-	for i := 0; i < n; i++ {
-		go sleep_and_wake_up(i, f_barrier, s_barrier, ch)
+		go sleep_and_wake_up(i, f_barrier, s_barrier, channels)
 	}
 	s_barrier.Wait()
 }
 
-func sleep_and_wake_up(id int, f_barrier, s_barrier *sync.WaitGroup, ch []chan int) {
-	first_phase(id, ch)
+func sleep_and_wake_up(id int, f_barrier, s_barrier *sync.WaitGroup, channels []chan int) {
+	first_phase(id, channels)
 	f_barrier.Done()
 	f_barrier.Wait()
-	second_phase(id, ch)
+	second_phase(id, channels)
 	s_barrier.Done()
 }
 
-func first_phase(id int, ch []chan int) {
-	sleep_time := generate_random_bounded_integer(6)
+func first_phase(id int, channels []chan int) {
+	sleep_time := generate_random_bounded_integer()
 	fmt.Printf("Thread-%d will sleep for %d seconds.\n", id, sleep_time)
 	time.Sleep(time.Duration(sleep_time) * time.Second)
 	fmt.Printf("Thread-%d has wake up.\n", id)
 
-	pred_index := find_predecessor_index(id, len(ch))
-	random_int := generate_random_bounded_integer(6)
-	ch[pred_index] <- random_int
-	fmt.Printf("Thread-%d set %d seconds for thread-%d.\n", id, random_int, pred_index)
+	pred_id := find_predecessor_id(id, len(channels))
+	pred_sleep_time := generate_random_bounded_integer()
+	channels[pred_id] <- pred_sleep_time
+	fmt.Printf("Thread-%d set %d seconds for thread-%d.\n", id, pred_sleep_time, pred_id)
 }
 
-func generate_random_bounded_integer(bound int) int {
-	random := rand.Intn(bound)
+func generate_random_bounded_integer() int {
+	random := rand.Intn(random_upper_bound)
 	return random
 }
 
-func second_phase(id int, ch []chan int) {
-	sleep_time := <-ch[id]
-	fmt.Printf("Thread %d will sleep, again. Now for (%d) seconds.\n", id, sleep_time)
+func second_phase(id int, channels []chan int) {
+	sleep_time := <-channels[id]
+	close(channels[id])
+	fmt.Printf("Thread %d will sleep, again. Now for %d seconds.\n", id, sleep_time)
 	time.Sleep(time.Duration(sleep_time) * time.Second)
 	fmt.Printf("Thread %d has wake up, again.\n", id)
 }
 
-func find_predecessor_index(current, n int) int {
-	index := current
-	if current == 0 {
-		index = n
+func find_predecessor_id(id, n int) int {
+	pred_id := id
+	if id == 0 {
+		pred_id = n
 	}
-	return index - 1
+	return pred_id - 1
 }
